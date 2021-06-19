2d51ea2364212e85d7e334576e29b5e3faf31493
bulish <babickovalibuse@seznam.cz>
2021-06-19 22:20:19 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.scss';
+import { gsap, TweenMax } from 'gsap';
 
 function App() {
+  document.body.addEventListener('mousemove', function(e) {
+    TweenMax.to('.cursor__ball', 0.4, {
+      x: e.pageX - 15,
+      y: e.pageY - 15,
+    });
+  });
+
+  const mouseEnterFunction = () => {
+    gsap.to(".cursor", {display: "block"});
+  }
+  const mouseLeaveFunction = () => {
+			gsap.to('.cursor', { display: 'none' });
+	};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<main>
+			<section onMouseEnter={mouseEnterFunction} onMouseLeave={mouseLeaveFunction}>
+				<h1>
+					Cursor Effect
+				</h1>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga ad
+					sequi, voluptate ea maiores quo placeat amet dicta officia, sed
+					eveniet molestiae asperiores est sint ut similique. Perspiciatis
+					placeat dolore mollitia voluptas voluptates ratione cum deserunt
+					dolorem nam quam ipsa soluta nulla culpa suscipit nemo, excepturi
+					sequi hic officia magni.
+				</p>
+			</section>
+			<div className="cursor">
+				<div className="cursor__ball ">
+					<svg height="100" width="100">
+						<circle cx="50" cy="50" r="40" strokeWidth="0"></circle>
+					</svg>
+				</div>
+			</div>
+		</main>
+	);
 }
 
 export default App;
